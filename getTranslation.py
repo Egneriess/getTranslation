91667d9b4387e219493c398a53ab21ded99b7c65
liyupian <108216173+liyupian@users.noreply.github.com>
2022-10-01 16:47:59 +0800

    Update getTranslation.py

--- getTranslation.py
+++ getTranslation.py
@@ -1,10 +1,13 @@
 from requests import get
 import json
+import sys
 
 
 def Translation(s):
-    j = json.loads(get("http://fanyi.youdao.com/translate?&doctype=json&i="+s).text)
-    result = j['translateResult'][0][0]['tgt']
-    return result
+    j = json.loads(get("http://fanyi.youdao.com/translate?&doctype=json&i=" + s).text)
+    return j['translateResult'][0][0]['tgt']
 
-print("翻译后的结果:"+Translation(input("请输入需要翻译的文字:")))
+try:
+    print("翻译后的结果:" + Translation(sys.argv[1]))
+except:
+    print("翻译后的结果:"+ Translation(input("请输入需要翻译的文字:")))
